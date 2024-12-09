6dbeef9a520c16f91f60346ab073c05436e9c805
Romain Portier <romain.portier@wanadoo.fr>
2024-12-09 15:56:03 +0100

    index => show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :get_movie, only: [:show, :edit, :destroy]
+
   def index
     @movies = Movie.all
   end
@@ -12,6 +14,16 @@ class MoviesController < ApplicationController
   def edit
   end
 
-  def delete
+  def destroy
+  end
+
+  private
+
+  def get_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :genre, :director_id)
   end
 end
